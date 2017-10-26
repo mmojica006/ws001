c76f706530a1682246646e9d66ef19d7af136706
mmojica006 <musicmojica@hotmail.com>
2017-10-25 20:51:34 -0600

    Add files via upload

---
+++ views/modules/publicidad.php
@@ -0,0 +1,19 @@
+    <section class="agencia py-1" id="agencia">
+
+        <div class="container">
+
+
+            <div class="row">
+
+                <div class="col-md-8 col-xl-9 wow bounceIn" data-wow-delay=".3s">
+                    <h2 class="h3 text-xs-center text-md-left font-weight-bold">Únete al Equipo!</h2>
+                    <p>Si eres un profesional líder y estás comprometido con la excelencia, envíanos tu currículo actualizado a info@crediexpress.com.ni y postula para un puesto de trabajo en Crediexpress.</p>
+
+                </div>
+                <div class="col-md-4 col-xl-3 wow bounceIn" data-wow-delay=".6s">
+                    <img src="views/images/familia.png" alt="La agencia">
+                </div>
+            </div>
+        </div>
+
+    </section>

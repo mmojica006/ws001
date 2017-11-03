303092bb140928dc96917708bf1ac859a47a0bc6
MOISES AMIN MOJICA RIVAS <mmojica@seintecnicsa.com>
2017-11-03 15:38:26 -0600

    changin index

--- admbackend/index.php
+++ admbackend/index.php
@@ -1,11 +1,7 @@
 <?php
 
-require_once "models/enlaces.php";
-require_once "models/ingreso.php";
+require_once "controllers/plantilla.controlador.php";
 
-require_once "controllers/template.php";
-require_once "controllers/enlaces.php";
-require_once "controllers/ingreso.php";
 
-$template = new TemplateController();
-$template->template();
+$plantilla = new ControladorPlantilla();
+$plantilla->plantilla();
